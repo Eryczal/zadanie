4c12c418967843ca4de2a06a524882eee90fb684
Eryczal <79263592+Eryczal@users.noreply.github.com>
2024-11-13 20:09:38 +0100

    Write app logic

---
+++ index.js
@@ -0,0 +1,10 @@
+import "dotenv/config.js";
+import { AiManager } from "./utils/AiManager.js";
+import { FileManager } from "./utils/FileManager.js";
+
+const aiManager = new AiManager();
+const fileManager = new FileManager();
+
+const data = await fileManager.loadText();
+const response = await aiManager.sendMessage(data);
+fileManager.generateFile(response);
